00cd35974f331f6cd3da5dbe27ef41cd90f2a118
Marcin Piwek <marcin.piwek@unitedideas.pl>
2017-07-06 19:18:46 +0200

    custom method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before do
+    @user=User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Marcin", last_name: "Piwek")
+  end
   describe "creation" do
-    before do
-      @user=User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Marcin", last_name: "Piwek ")
-    end
     it "can be created" do
       expect(@user).to be_valid
     end
@@ -12,6 +12,11 @@ RSpec.describe User, type: :model do
         @user.first_name=nil
         @user.last_name=nil
         expect(@user).to_not be_valid
+    end
+  end
+  describe "custom name methods" do
+    it 'has a full name method' do
+      expect(@user.full_name).to eq("PIWEK, MARCIN")
     end
   end
 end
