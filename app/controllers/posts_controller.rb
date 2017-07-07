6bb6eb4e39f0fdbf1e8407dbbe93fbad2919c857
Marcin Piwek <marcin.piwek@unitedideas.pl>
2017-07-07 10:50:36 +0200

    edit and update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,19 @@
 class PostsController < ApplicationController
-	before_action :set_post, only: [:show]
+	before_action :set_post, only: [:show, :edit, :update]
 
 	def index
 		@posts = Post.all
+	end
+
+	def edit
+
+	end
+	def update
+		if @post.update(post_params)
+		redirect_to @post, notice: 'Your post was updated successfully'
+	else
+		render :edit
+	end
 	end
 
 	def new
