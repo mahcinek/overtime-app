a3d5631da272c33c1b1998b002f848900871117e
Marcin Piwek <marcin.piwek@unitedideas.pl>
2017-07-11 09:23:53 +0200

    validation and default for audit log

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,10 @@ puts '2 users created'
 10.times do |post|
   Post.create!(date: Date.today, rationale: "#{post}, rationale content", user_id: @user.id, status: 0, overtime_request: 2.5)
 end
+10.times do |audit_log|
+  AuditLog.create!(date: Date.today, user_id: User.last.id, status: 0, start_date:(Date.today - 6.days))
+end
+pusts "10 audit logs"
 10.times do |post|
   Post.create!(date: Date.today, rationale: "#{post}, rationale content", user_id: @user.id, status: 1, overtime_request: 1.5)
 end
