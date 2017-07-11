cda7adb1cb76db0fa5c8fdb1315ea49bf9bd48fa
Marcin Piwek <marcin.piwek@unitedideas.pl>
2017-07-11 13:32:54 +0200

    employee homepage start

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,9 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
 rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+def admin_types
+  ['AdminUser']
+end
 
 private
 
--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,10 @@
 class StaticController < ApplicationController
   def home
-    @pending_approvals=Post.where(status: 'submited')
+    if admin_types.include?(current_user.type)
+    @pending_approvals=Post.submited
     @recent_audit_items=AuditLog.last(10)
+  else
+    #sth else
+  end
   end
 end
