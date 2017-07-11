dc8f2b8ea16650561ff4d44ffc3b952574a243f0
Marcin Piwek <marcin.piwek@unitedideas.pl>
2017-07-11 14:29:44 +0200

    employee homepage conformation

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :audit_logs, except: [:new, :edit, :destroy]
+  resources :audit_logs, except: [:new, :edit, :destroy] do
+    member do
+      get :confirm
+    end
+  end
   resources :posts do
     member do
       get :approve
--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -10,4 +10,15 @@ describe 'Homepage' do
     expect(post.reload.status).to eq('approved')
 
   end
+  it 'allows the employee to change audit status from homepage' do
+    audit_log=FactoryGirl.create(:audit_log)
+    @user = FactoryGirl.create(:user)
+    user=@user
+    audit_log.update(user_id: user.id)
+    login_as(user, :scope => :user)
+    visit root_path
+    click_on("confirm_#{audit_log.id}")
+    expect(audit_log.reload.status).to eq('confirmed')
+
+  end
 end
