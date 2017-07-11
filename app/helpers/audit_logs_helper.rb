f75712931750d6207080436dd3a8f1c86a0de99d
Marcin Piwek <marcin.piwek@unitedideas.pl>
2017-07-11 10:49:14 +0200

    audit log dashboard and enum

--- app/helpers/audit_logs_helper.rb
+++ app/helpers/audit_logs_helper.rb
@@ -1,2 +1,11 @@
 module AuditLogsHelper
-end
+
+    def status_label_audit status
+      case status
+      when 'pending'
+        content_tag(:span, status.titleize, class: 'label label-primary')
+      when 'confirmed'
+        content_tag(:span, status.titleize, class: 'label label-success')
+      end
+    end
+  end
