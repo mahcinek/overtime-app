4cfcaf526b684bf2d198ac3726037e8653e6600a
Marcin Piwek <marcin.piwek@unitedideas.pl>
2017-07-11 11:05:39 +0200

    pagination

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,7 @@ puts '2 users created'
 10.times do |post|
   Post.create!(date: Date.today, rationale: "#{post}, rationale content", user_id: @user.id, status: 0, overtime_request: 2.5)
 end
-10.times do |audit_log|
+12.times do |audit_log|
   AuditLog.create!(start_date: Date.today, user_id: @user.id, status: 0, start_date:(Date.today - 6.days))
 end
 puts "10 audit logs"
